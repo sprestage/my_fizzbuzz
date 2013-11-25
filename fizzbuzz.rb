c5088d925b6e378bfc5078b6781cadbdf93b05cb
Susan Prestage <susan.prestage@gmail.com>
2013-11-25 10:38:42 -0800

    fixing my output

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,12 +7,13 @@ def fizzbuzz (num, div_by_num, output_string)
 end
 
 (-50..50).each do |x|
-
-  a = fizzbuzz(x, 3, "fizz")
-  puts a
-  b = fizzbuzz(x, 5, "buzz")
-  puts b
-  c = fizzbuzz(x, 7, "sivv")
-  puts c
+  output = ""
+  output << fizzbuzz(x, 3, "fizz").to_s
+  output << fizzbuzz(x, 5, "buzz").to_s
+  output << fizzbuzz(x, 7, "sivv").to_s
+  if output == ""
+    puts x
+  else
+    puts output
+  end
 end
-
